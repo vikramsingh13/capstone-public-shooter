951b8ecbf9cd68106abea75825b35b008d262fa9
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2023-10-31 01:40:02 -0400

    Plasma Gun Fixed and Ready

--- WiseRoguelikeFPS/Assets/Scripts/Controller/BulletController.cs
+++ WiseRoguelikeFPS/Assets/Scripts/Controller/BulletController.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using Zenject;
 
 public class BulletController : MonoBehaviour
 {
@@ -12,23 +9,12 @@ public class BulletController : MonoBehaviour
     public float bulletLife;
     public int bulletDamage;
 
+    public bool hasAreaOfEffect;
+    public float splashDamage;
+    public float splashDistance;
+
+
     public Rigidbody bulletBody;
-
-    [SerializeField]
-    private GameManager _gameManager;
-
-
-    [Inject]
-    public void Construct(GameManager gameManager)
-    {
-        _gameManager = gameManager;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -53,36 +39,66 @@ public class BulletController : MonoBehaviour
     {
         if(other.CompareTag("Enemy"))
         {
+
+            if (hasAreaOfEffect)
+            {
+
+                DealSplashDamage();
+
+            }
+
             EnemyHealth target = other.transform.GetComponent<EnemyHealth>();  // Get the EnemyHealth component of the hit object
             target.TakeDamage(bulletDamage);
         }
         Destroy(this);
     }
-    public void LoadProjectile(string projectileName, AssetReference projectileReference, GameObject source, Transform pos, Vector3 targetPos, bool isFiredByPlayer)
+
+    private void OnCollisionEnter(Collision collision)
     {
-        if (_gameManager != null && !_gameManager.IsPaused)
+
+        //If the projectile hit an enemy.
+        if (collision.gameObject.tag == ("Enemy"))
         {
-            AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>(projectileReference);
-            handle.Completed += (operation) => OnProjectileLoaded(operation, projectileName, source, pos, targetPos, isFiredByPlayer);
+
+            //Do damage.
+            EnemyHealth target = collision.transform.GetComponent<EnemyHealth>();  // Get the EnemyHealth component of the hit object
+            target.TakeDamage(bulletDamage);
+            //Destroy(this.gameObject);
+
         }
-        else
+
+        //If it does AoE damage, deal it out.
+        if (hasAreaOfEffect)
         {
-            Debug.LogError($"Projectile data not found for projectile name: {projectileName}");
+
+            DealSplashDamage();
+
         }
+
+        //Destroy the projectile at the end.
+        Destroy(this.gameObject);
+
     }
 
-    private void OnProjectileLoaded(AsyncOperationHandle<GameObject> handle, string projectileName, GameObject source, Transform pos, Vector3 targetPos, bool isFiredByPlayer)
+    private void DealSplashDamage()
     {
-        if (handle.Status == AsyncOperationStatus.Succeeded)
-        {
-            //enemy object could have been destroyed by the time this is called
-            //no need to check if the Transform ref exists or is it null
-            if (pos != null)
+
+        //Get the colliders of everything within [splashDistance] meters of the impact point.
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position, splashDistance);
+        
+        foreach (Collider c in colliders) { 
+        
+            //If a collider has an EnemyHealth component.
+            if(c.GetComponent<EnemyHealth>())
             {
-                Projectile projectile = Instantiate(handle.Result, pos.position, pos.rotation).GetComponent<Projectile>();
-                //projectile.Init(false, projectileName, source, 10, 2, targetPos, isFiredByPlayer);
-                projectile.Fire();
+
+                //Deal splash damage.
+                c.GetComponent<EnemyHealth>().TakeDamage(splashDamage);
+
             }
+        
         }
+
     }
+
 }
