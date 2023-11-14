8000c51f7c30a45447e4de4acbb9866b6928cd2a
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-11-13 22:37:50 -0500

    working audio settings. Switching to test scene now from Play

---
+++ WiseRoguelikeFPS/Assets/Scripts/Data/SettingsData.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SettingsData", menuName = "ScriptableObjects/SettingsData")]
+public class SettingsData : ScriptableObject
+{
+    public float mouseSensitivity = 100f;
+    public float volumeMaster = 1f;
+    public float volumeMusic = 1f;
+    public float volumeAmbience = 1f;
+    public float volumeEffects = 1f;
+
+    /* 
+    public float fov = 90f;
+    public bool invertY = false;
+    public bool invertX = false;
+    public bool isFullscreen = true;
+    public int resolutionIndex = 0;
+    public int qualityIndex = 0;
+    public int textureQualityIndex = 0;
+    public int antialiasingIndex = 0;
+    public int vSyncIndex = 0;
+    public int anisotropicFilteringIndex = 0;
+    public int shadowResolutionIndex = 0;
+    public int shadowCascadesIndex = 0;
+    public int blendWeightsIndex = 0;
+    public int softParticlesIndex = 0;
+    public int realtimeReflectionProbesIndex = 0;
+    public int billboardsFaceCameraPositionIndex = 0;
+    public int lodBiasIndex = 0;
+    public int maximumLODLevelIndex = 0;
+    public int particleRaycastBudgetIndex = 0;
+    public int asyncUploadTimeSliceIndex = 0;
+    public int asyncUploadBufferSizeIndex = 0;
+    public int asyncUploadPersistentBufferIndex = 0;
+    public int resolutionWidth = 1920;
+    public int resolutionHeight = 1080;
+    public int refreshRate = 60;
+    public int targetFrameRate = 60;
+    public int vSyncCount = 0;
+    public int anisotropicFiltering = 0;
+    public int shadowResolution = 0;
+    public int shadowCascades = 0;
+    public int blendWeights = 0;
+    public int softParticles = 0;
+    public int realtimeReflectionProbes = 0;
+    public int billboardsFaceCameraPosition = 0;
+    public int lodBias = 0;
+    public int maximumLODLevel = 0;
+    */
+}
---
+++ WiseRoguelikeFPS/Assets/Scripts/Model/Menu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Zenject;
+
+public class Menu : MonoBehaviour
+{
+    //Menu panel
+    private GameObject _menuPanel;
+    //Options panel
+    private GameObject _optionsPanel;
+
+    //MusicVolumeSlider
+    private Slider _musicSlider;
+    //AmbienceVolumeSlider
+    private Slider _ambienceSlider;
+    //EffectsVolumeSlider
+    private Slider _effectsSlider;
+
+    //volume changed events from audio manager
+    public static event AudioManager.MusicVolumeChangedEvent MusicVolumeChangedEvent;
+    public static event AudioManager.AmbienceVolumeChangedEvent AmbienceVolumeChangedEvent;
+    public static event AudioManager.EffectsVolumeChangedEvent EffectsVolumeChangedEvent;
+
+    void Start()
+    {
+        //find the gameObject with "MenuPanel" name and set it to the _menuPanel
+        //this way both mainMenu and ingameMenu can be handled with the same script
+        _menuPanel = GameObject.Find("MenuPanel");
+        //find the gameObject with "OptionsPanel" name and set it to the _optionsPanel
+        _optionsPanel = GameObject.Find("OptionsPanel");
+
+
+        //find the gameObjects named Start, Options and Exit buttons and add event listeners to them
+        GameObject.Find("StartButton").GetComponent<Button>().onClick.AddListener(PlayGame);
+        GameObject.Find("OptionsButton").GetComponent<Button>().onClick.AddListener(ToggleOptions);
+        GameObject.Find("ExitButton").GetComponent<Button>().onClick.AddListener(QuitGame);
+
+        //add eventlisteners to the sliders and invoke Audio Manager events on value change
+        _musicSlider = GameObject.Find("MusicVolumeSlider").GetComponent<Slider>();
+        _ambienceSlider = GameObject.Find("AmbienceVolumeSlider").GetComponent<Slider>();
+        _effectsSlider = GameObject.Find("EffectsVolumeSlider").GetComponent<Slider>();
+
+        _musicSlider.onValueChanged.AddListener(delegate { MusicVolumeChangedEvent?.Invoke(_musicSlider.value); });
+        _ambienceSlider.onValueChanged.AddListener(delegate { AmbienceVolumeChangedEvent?.Invoke(_ambienceSlider.value); });
+        _effectsSlider.onValueChanged.AddListener(delegate { EffectsVolumeChangedEvent?.Invoke(_effectsSlider.value); });
+
+
+        //add an onclick event to the back button in the options panel
+        GameObject.Find("OptionsBackButton").GetComponent<Button>().onClick.AddListener(ToggleOptions);
+
+        ToggleOptions();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("TestFirstLevelScene");
+    }
+
+    //toggle the options panel
+    public void ToggleOptions()
+    {
+        Debug.Log("Before toggle " + _optionsPanel.activeSelf);
+        _optionsPanel.SetActive(!_optionsPanel.activeSelf);
+
+        Debug.Log("After toggle: " + _optionsPanel.activeSelf);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
